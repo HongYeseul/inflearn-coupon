ecdd43ab93d0619524b16970ba2ac5be89e2d73f
HongYeseul <ghd8119@naver.com>
2024-09-26 10:16:08 +0900

    :tada: git init

---
+++ api/src/test/java/com/example/api/service/ApplyServiceTest.java
@@ -0,0 +1,56 @@
+package com.example.api.service;
+
+import com.example.api.repository.CouponRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class ApplyServiceTest {
+
+    @Autowired
+    private ApplyService applyService;
+
+    @Autowired
+    private CouponRepository couponRepository;
+
+    @Test
+    public void 한번만_응모() {
+        applyService.apply(1L);
+
+        long count = couponRepository.count();
+
+        assertThat(count).isEqualTo(1);
+    }
+
+    @Test
+    public void 여러명_응모() throws InterruptedException {
+        int threadCount = 1000;
+        ExecutorService executorService = Executors.newFixedThreadPool(32);
+        CountDownLatch latch = new CountDownLatch(threadCount); // 다른 작업이 끝날 때까지 기다려주는 클래스
+
+        for (int i = 0; i < threadCount; i++) {
+            long userId = i;
+            executorService.execute(() -> {
+                try {
+                    applyService.apply(userId);
+                } finally {
+                    latch.countDown();
+                }
+            });
+        }
+
+        latch.await();
+
+        long count = couponRepository.count();
+
+        assertThat(count).isEqualTo(100);
+    }
+}
